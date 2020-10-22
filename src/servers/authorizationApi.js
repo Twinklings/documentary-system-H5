f22ccc1816d888f10c0f7b241ca2bee6edfcc42a
谢文欣 <yf_xiewenxin@unisinsight.com>
2020-10-22 11:37:39 +0800

    修改接口IP地址
    修改接口IP地址

--- src/servers/authorizationApi.js
+++ src/servers/authorizationApi.js
@@ -1,7 +1,8 @@
 import request from '../utils/axios'
 import { conversionFormat } from '../utils/utils'
 
-let serverIP = "http://47.102.204.79:80";
+// let serverIP = "http://47.102.204.79:80";
+let serverIP = "http://h5.genleme.com:80";
 
 export function getWeChatConfig(params) {
     return request({
