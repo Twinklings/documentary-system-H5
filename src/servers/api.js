4ae904a62e8b7717ab9516ada92b8a00ea195dcf
danfuman <1169694155@qq.com>
2021-05-22 19:55:08 +0800

    修改

--- src/servers/api.js
+++ src/servers/api.js
@@ -59,7 +59,7 @@ export function viewLogistics(params) {
 //意见反馈
 export function submitFeedBack(params) {
     return request({
-        url:serverIP+"portal/complaintsSave",
+        url:serverIP+"/portal/complaintsSave",
         method:'post',
         data:params
     })
